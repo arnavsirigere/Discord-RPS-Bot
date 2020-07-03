285112cbf3e784404af58e883654ab68e2fb2726
Arnav Sirigere <arnavsirigere@gmail.com>
2020-07-03 16:44:11 +0530

    Adding embed to rps! he;lp

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,21 +1,22 @@
 // Command for sending a message with instructions
 
-function help(message) {
-  let reply = `
-Here are the commands you can use - 
+const { MessageEmbed } = require('discord.js');
 
-${'`' + 'rps! score' + '`'} - To view your overall score and my score against you!
-${'`' + 'rps! [rock | paper | scissors]' + '`'} - To play a round with me!
-${'`' + 'rps! help' + '`'} - Well, what you just used.
-${'`' + 'rps! ping' + '`'} - Pong!
-${'`' + 'rps! cheat?' + '`'} - I promise I don't cheat! 😊
-${'`' + 'rps! rules' + '`'} - I'll tell you the rules of the game
-${'`' + 'rps! leaderboard' + '`'} - I'll give you the entire leaderboard
-${'`' + 'rps! leaderboard me' + '`'} - I'll tell you what place you stand at in the leaderboard
-${'`' + 'rps! leaderboard [n]' + '`'} - I'll tell you the top n players in the leaderboard!
-${'`' + 'rps! congrats' + '`'} - 'Thank You 😊'
-`;
-  message.channel.send(reply);
+function help(message) {
+  const embed = new MessageEmbed()
+    .setColor('#64E9EE')
+    .setTitle('Here are the commands you can use  - ')
+    .addField('`' + 'rps! [rock | paper | scissors]' + '`', 'To play a round with me!')
+    .addField('`' + 'rps! score' + '`', 'To view your overall score and my score against you!')
+    .addField('`' + 'rps! rules' + '`', "I'll tell you the rules of the game!")
+    .addField('`' + 'rps! leaderboard me' + '`', "I'll tell you what place you stand at in the leaderboard!")
+    .addField('`' + 'rps! leaderboard' + '`', "I'll give you the entire leaderboard!")
+    .addField('`' + 'rps! leaderboard [n]' + '`', "I'll tell you the top n players in the leaderboard!")
+    .addField('`' + 'rps! help' + '`', 'Well, what you just used!')
+    .addField('`' + 'rps! cheat?' + '`', "I promise I don't cheat! 😊")
+    .addField('`' + 'rps! ping' + '`', 'Pong!')
+    .addField('`' + 'rps! congrats' + '`', 'Thank You 😊');
+  message.channel.send(embed);
 }
 
 module.exports = help;
