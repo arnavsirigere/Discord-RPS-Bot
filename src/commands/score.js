84ace380299c61a075cd14cd8a0809941e3bf60d
Arnav Sirigere <arnavsirigere@gmail.com>
2020-07-03 21:16:06 +0530

    Adding embed for rps! score

--- src/commands/score.js
+++ src/commands/score.js
@@ -1,13 +1,18 @@
 // Command for telling the user his score and the ai's score against him
 
+const { MessageEmbed } = require('discord.js');
+
 function tellScore(userData, message) {
-  let { aiScore, userScore } = userData;
-  let userIsWinner = userScore >= aiScore;
-  let reply = `
-  ${userIsWinner ? `Your score against me : ${'`' + userScore + '`'}` : `My Score against you  : ${'`' + aiScore + '`'}`}
-  ${userIsWinner ? `My Score against you   : ${'`' + aiScore + '`'}` : `Your score against me : ${'`' + userScore + '`'}`}
-  ${aiScore > userScore ? 'I am winning! 😁' : userScore > aiScore ? 'You are winning! 😓' : 'We are all tied up! 🙌'}`;
-  message.reply(reply);
+  const { aiScore, userScore } = userData;
+  const userIsWinner = userScore >= aiScore;
+  const title = `${aiScore > userScore ? 'I am winning! 😁' : userScore > aiScore ? 'You are winning! 😓' : 'We are all tied up! 🙌'}`;
+  const embed = new MessageEmbed()
+    .setColor('#5603AD')
+    .setTitle(title)
+    .addField(`${userIsWinner ? 'Your score' : 'My score'}`, userIsWinner ? userScore : aiScore)
+    .addField(`${userIsWinner ? 'My score' : 'Your score'}`, userIsWinner ? aiScore : userScore);
+
+  message.reply(embed);
 }
 
 module.exports = tellScore;
